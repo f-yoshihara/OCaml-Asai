b49d8c7eb999aec7170e1eb561e17431a295738a
f-yoshihara <fumiro1989@gmail.com>
2021-07-23 22:39:25 +0900

    演習問題

--- chapter5/kion.ml
+++ chapter5/kion.ml
@@ -1,12 +1,21 @@
-(* 目的 : 現在気温から快適度を返す*)
-(* kion : int -> strign*)
+(* 目的 : 現在気温から快適度を返す *)
+(* kion : int -> strign *)
 let kion t =
-  if t < 15 then "普通"
-            else if t <= 25 then "快適"
-                            else "普通"
+  if kaiteki t then "快適"
+               else "普通"
 (* test *)
 let test1 = kion 7  = "普通"
 let test2 = kion 15 = "快適"
 let test3 = kion 20 = "快適"
 let test4 = kion 25 = "快適"
 let test5 = kion 28 = "普通"
+
+(* 目的 : 気温が15以上25以下かどうかを判断する *)
+(* kaiteki : int -> bool *)
+let kaiteki t = 15 <= t && t <= 25
+(* test *)
+let test6  = kaiteki 14 = false
+let test7  = kaiteki 15 = true
+let test8  = kaiteki 20 = true
+let test9  = kaiteki 25 = true
+let test10 = kaiteki 26 = false
--- chapter5/practice.ml
+++ chapter5/practice.ml
@@ -6,5 +6,49 @@ let jikan x =
 (* test *)
 let test1 = jikan 0  = "before"
 let test2 = jikan 11 = "before"
-let test2 = jikan 12 = "after"
-let test1 = jikan 23 = "after"
+let test3 = jikan 12 = "after"
+let test4 = jikan 23 = "after"
+
+(* 目的 : ax2 + bx + c = 0 の係数 a, b, c を受け取って判別式の値を返す *)
+(* hanbetsusiki : float -> float -> float -> float *)
+let hanbetsusiki a b c = b ** 2. -. 4. *. a *. c
+(* test *)
+let test5  = hanbetsusiki 1. 1. 1.  = -3.
+let test6  = hanbetsusiki 1. 2. 3.  = -8.
+let test7  = hanbetsusiki 1. (-5.) 4. = 9.
+let test8  = hanbetsusiki 2. (-6.) 4. = 4.
+let test9  = hanbetsusiki 2. 0. 4.  = -32.
+let test10 = hanbetsusiki 2. 4. 0.  = 16.
+let test11 = hanbetsusiki 4. 4. 1.  = 0.
+let test12 = hanbetsusiki 1. 4. 4.  = 0.
+
+(* 目的 : ax2 + bx + c = 0 の係数 a, b, c を受け取って解の個数を返す *)
+(* kai_no_kosuu : float -> float -> float -> int *)
+let kai_no_kosuu a b c =
+  if hanbetsusiki a b c < 0. then 0
+                             else if hanbetsusiki a b c = 0. then 1
+                                                             else 2
+(* test *)
+let test13 = kai_no_kosuu 1. 1. 1.  = 0
+let test14 = kai_no_kosuu 1. 2. 3.  = 0
+let test15 = kai_no_kosuu 1. (-5.) 4. = 2
+let test16 = kai_no_kosuu 2. (-6.) 4. = 2
+let test17 = kai_no_kosuu 2. 0. 4.  = 0
+let test18 = kai_no_kosuu 2. 4. 0.  = 2
+let test19 = kai_no_kosuu 4. 4. 1.  = 1
+let test20 = kai_no_kosuu 1. 4. 4.  = 1
+
+(* 目的 : ax2 + bx + c = 0 の係数 a, b, c を受け取って虚数解を持つか否かを判定する *)
+(* kyosuukai : float -> float -> float -> bool *)
+let kyosuukai a b c =
+  if hanbetsusiki a b c < 0. then true
+                             else false
+(* test *)
+let test21 = kyosuukai 1. 1. 1.  = true
+let test22 = kyosuukai 1. 2. 3.  = true
+let test23 = kyosuukai 1. (-5.) 4. = false
+let test24 = kyosuukai 2. (-6.) 4. = false
+let test25 = kyosuukai 2. 0. 4.  = true
+let test26 = kyosuukai 2. 4. 0.  = false
+let test27 = kyosuukai 4. 4. 1.  = false
+let test28 = kyosuukai 1. 4. 4.  = false
