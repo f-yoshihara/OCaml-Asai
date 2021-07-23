86938393382e30a7efad081a861c76044e05c6d9
f-yoshihara <fumiro1989@gmail.com>
2021-07-23 18:50:47 +0900

    鶴亀算

--- chapter4/practice.ml
+++ chapter4/practice.ml
@@ -21,3 +21,13 @@ let tsurukame_no_ashi tsuru kame = tsuru_no_ashi tsuru + kame_no_ashi kame
 let test7 = tsurukame_no_ashi 0 0 = 0
 let test8 = tsurukame_no_ashi 1 1 = 6
 let test9 = tsurukame_no_ashi 2 3 = 16
+
+(* 目的 : 鶴亀算。鶴と亀の合計とそれらの足の合計から鶴の数を返す *)
+(* tsurukame : int -> int -> int *)
+let tsurukame hiki ashi = (4 * hiki - ashi) / 2
+(* test *)
+let test10 = tsurukame 2 6 = 1
+let test11 = tsurukame 4 10 = 3
+let test12 = tsurukame 12 28 = 10
+let test13 = tsurukame 2 4 = 2
+let test14 = tsurukame 2 8 = 0
