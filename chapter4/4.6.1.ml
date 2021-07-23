44088aa8b090048763125b6ae0ca54faa688ccdd
f-yoshihara <fumiro1989@gmail.com>
2021-07-23 18:27:26 +0900

    ファイルをまとめて関数を再利用できるようにした

--- chapter4/4.6.1.ml
+++
@@ -1,8 +0,0 @@
-(* 目的 : 鶴の数を与えられてそれらの足の数を返す *)
-(* tsuru_no_ashi : int -> int *)
-let tsuru_no_ashi x = x * 2
-
-(* test *)
-let test1 = tsuru_no_ashi 1 = 2
-let test4 = tsuru_no_ashi 4 = 8
-let test9 = tsuru_no_ashi 9 = 18
---
+++ chapter4/practice.ml
@@ -0,0 +1,23 @@
+(* 目的 : 鶴の数を与えられてそれらの足の数を返す *)
+(* tsuru_no_ashi : int -> int *)
+let tsuru_no_ashi x = x * 2
+(* test *)
+let test1 = tsuru_no_ashi 1 = 2
+let test2 = tsuru_no_ashi 4 = 8
+let test3 = tsuru_no_ashi 9 = 18
+
+(* 目的 : 亀の数を与えられて足の本数を返す *)
+(* kame_no_ashi : int -> int *)
+let kame_no_ashi x = x * 4
+(* test *)
+let test4 = kame_no_ashi 1 = 4
+let test5 = kame_no_ashi 2 = 8
+let test6 = kame_no_ashi 9 = 36
+
+(* 目的 : 鶴と亀の数からそれらの足の数の合計を返す *)
+(* tsurukame_no_ashi : int -> int -> int *)
+let tsurukame_no_ashi tsuru kame = tsuru_no_ashi tsuru + kame_no_ashi kame
+(* test *)
+let test7 = tsurukame_no_ashi 0 0 = 0
+let test8 = tsurukame_no_ashi 1 1 = 6
+let test9 = tsurukame_no_ashi 2 3 = 16
