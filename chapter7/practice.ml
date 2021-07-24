9a549b50c7443d9f81fb580d63faeeca74460d5f
f-yoshihara <fumiro1989@gmail.com>
2021-07-24 15:00:20 +0900

    practicing pattern matching.

--- chapter7/practice.ml
+++ chapter7/practice.ml
@@ -11,7 +11,15 @@ let test4 = goukei 2 4 6 8 9 = 29
 (* goukei_to_heikin : int -> int -> int -> int -> int -> (int * float)*)
 let goukei_to_heikin a b c d e = (goukei a b c d e, (float (goukei a b c d e)) /. 5.)
 (* test *)
-let test1 = goukei_to_heikin 0 0 0 0 0 = (0, 0.)
-let test2 = goukei_to_heikin 1 1 1 1 1 = (5, 1.)
-let test3 = goukei_to_heikin 1 2 3 4 5 = (15, 3.)
-let test4 = goukei_to_heikin 2 4 6 8 9 = (29, 5.8)
+let test5 = goukei_to_heikin 0 0 0 0 0 = (0, 0.)
+let test6 = goukei_to_heikin 1 1 1 1 1 = (5, 1.)
+let test7 = goukei_to_heikin 1 2 3 4 5 = (15, 3.)
+let test8 = goukei_to_heikin 2 4 6 8 9 = (29, 5.8)
+
+(* 目的 : 名前と成績の組を受け取ってそれらを通知する文字列を返す *)
+(* seiseki : (string, int) -> string *)
+let seiseki score = match score with (name, grade) -> name ^ "さんの成績は" ^ grade ^ "です"
+(* test *)
+let test9  = seiseki ("A", "A") = "Aさんの成績はAです"
+let test10 = seiseki ("B", "S") = "Bさんの成績はSです"
+let test11 = seiseki ("吉原", "C") = "吉原さんの成績はCです"
