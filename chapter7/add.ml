0cefaf7578afb04aae3dc7122a56b609fd376eb6
f-yoshihara <fumiro1989@gmail.com>
2021-07-25 13:44:11 +0900

    add 完成

--- chapter7/add.ml
+++ chapter7/add.ml
@@ -1,10 +1,8 @@
 (* 目的 : 二つの整数の組を受け取って要素の和を返す *)
 (* add : int * int -> int *)
 let add pair = match pair with
-  (a, b) -> 0
+  (a, b) -> a + b
 (* test *)
 let test1 = add (0, 0) = 0
 let test2 = add (3, 5) = 8
 let test3 = add (3, -5) = -2
-(*  *)
-(*  *)
